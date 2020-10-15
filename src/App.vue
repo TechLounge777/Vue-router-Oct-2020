f7dde4195581667629f90b5a36edf7a78ca71df8
TechLounge777 <msblanche777@gmail.com>
2020-10-15 10:26:36 +0200

    vue router

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/user/123">User</router-link></li>
+      <li><router-link to="/tasks">Tasks</router-link></li>
+    </ul>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
---
+++ src/components/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+  </ul>
+</template>
+
+<script>
+export default {
+name: "Tasks"
+}
+</script>
+
+<style scoped>
+    body {
+        color: blueviolet;
+    }
+</style>
